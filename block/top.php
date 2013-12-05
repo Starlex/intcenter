e19412d6553f0c6f85b78e361548ec04fb63db57
Starostenko Aleksey <staralforwork@gmail.com>
2013-12-05 15:53:46 +0600

    finished horizontal menu

--- block/top.php
+++ block/top.php
@@ -14,5 +14,27 @@
 	<div class="maindiv">
 		<div class="shadowleft"></div>
 		<div class="shadowright"></div>
-
-		<?php drawHorizontalMenu($db); ?>
+		<div class='h-menu'>
+    		<ul>
+    			<li>
+            		<a href=''><img src='../img/foreign_lang.png' alt='Изучение иностранных языков'></a>
+            		<a href=''>Изучение иностранных&nbsp;языков</a>
+        		</li>
+    			<li>
+		            <a href=''><img src='../img/visa_support.png' alt='Визовая поддержка'></a>
+					<a href=''>Визовая поддержка</a>
+		        </li>
+		    	<li>
+		            <a href=''><img src='../img/lang_schools.png' alt='Летние и зимние языковые школы'></a>
+		            <a href=''>Летние и зимние языковые школы</a>
+		        </li>
+		    	<li>
+		            <a href=''><img src='../img/prof_internships.png' alt='Профессиональные стажеровки за рубежом'></a>
+		            <a href=''>Профессиональные стажировки за рубежом</a>
+		        </li>
+		    	<li>
+		            <a href=''><img src='../img/doc_translate.png' alt='Перевод документов'></a>
+		            <a href=''>Перевод документов</a>
+        		</li>
+    		</ul>
+    	</div>
