d68b27e9c38b2de98818904ff8e5e7dd2300c811
Starostenko Aleksei <staralforwork@gmail.com>
2014-01-28 17:31:04 +0600

    Almost completed breadcrumbs

--- pages/news.php
+++ pages/news.php
@@ -9,11 +9,33 @@ try{
 catch(PDOException $e){
 	$e->getMessage();
 }
+$url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$uri = explode('/', $url);
+
+$arr = count($uri)-1;
+
+echo '<div class="breadcrumbs">';
+
+for ($i=0; $i < $arr; $i++) { 
+	if(0 === $i){
+		echo '<a href="/">Главная</a> ';
+		echo '<span>&gt;</span>';
+	}
+	elseif($arr-1 === $i){
+		echo date("d.m.Y", $data['date']);
+	}
+	else{
+		echo $uri[$i];
+		echo '<span>&gt;</span>';
+	}
+}
+
+echo '</div>';
 ?>
 
-<div class="breadcrumbs">
-	Главная > 13.01.2014
-</div>
+
+	<!-- <a href="/">Главная</a> <span>&gt;</span> 13.01.2014 -->
+
 
 <div class="container">
 	<?php drawProgramsMenu($db, $drawProgMenu)?>
