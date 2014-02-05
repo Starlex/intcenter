daa5371f500325020836b28088ed43c46a983fc7
Starostenko Aleksei <staralforwork@gmail.com>
2014-02-05 17:21:43 +0600

    Added img_resize()

--- block/phpfunlib.php
+++ block/phpfunlib.php
@@ -30,6 +30,7 @@ function cyrillic2latin($str){
 	return strtr($str, $converter);
 }
 
+
 function drawVerticalMenu($db, $isAdmin=0){
 	try{
 		$query = $db->prepare("SELECT COUNT(*) FROM intcenter_pages WHERE isAdmin=?");
@@ -340,20 +341,47 @@ function getPageContent($db, $link){
 	}
 }
 
+/*##################################   NOT MY FUNCTIONS  ###############################*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Source link http://forum.php.su/topic.php?forum=35&topic=12&postid=1176547253#1176547253
+function img_resize($src, $dest, $width, $height, $rgb=0xFFFFFF, $quality=100)
+{
+  if (!file_exists($src)) return false;
+ 
+  $size = getimagesize($src);
+ 
+  if ($size === false) return false;
+ 
+  $format = strtolower(substr($size['mime'], strpos($size['mime'], '/')+1));
+  $icfunc = "imagecreatefrom" . $format;
+  if (!function_exists($icfunc)) return false;
+ 
+  $x_ratio = $width / $size[0];
+  $y_ratio = $height / $size[1];
+ 
+  $ratio       = min($x_ratio, $y_ratio);
+  $use_x_ratio = ($x_ratio == $ratio);
+ 
+  $new_width   = $use_x_ratio  ? $width  : floor($size[0] * $ratio);
+  $new_height  = !$use_x_ratio ? $height : floor($size[1] * $ratio);
+  $new_left    = $use_x_ratio  ? 0 : floor(($width - $new_width) / 2);
+  $new_top     = !$use_x_ratio ? 0 : floor(($height - $new_height) / 2);
+ 
+  $isrc = $icfunc($src);
+  $idest = imagecreatetruecolor($width, $height);
+ 
+  imagefill($idest, 0, 0, $rgb);
+  imagecopyresampled($idest, $isrc, $new_left, $new_top, 0, 0,
+    $new_width, $new_height, $size[0], $size[1]);
+ 
+  imagejpeg($idest, $dest, $quality);
+ 
+  imagedestroy($isrc);
+  imagedestroy($idest);
+ 
+  return true;
+ 
+}
 
 /* get list of pages */
 function getPagesList($db){
