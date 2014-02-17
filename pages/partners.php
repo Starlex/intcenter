4cefbb5c6f1fed37b251996325b4e94458fa8b2e
Starostenko Aleksei <staralforwork@gmail.com>
2014-02-17 17:49:18 +0600

    Prototype of partners page

--- pages/partners.php
+++ pages/partners.php
@@ -8,6 +8,18 @@ catch(PDOException $e){
 	header('Location: /error/');
 }
 
-print_r($row);
 ?>
-<div class="container"></div>
+<div class="container">
+	<?php
+	echo '<h2>'.getPageName($db).'</h2>';
+	foreach ($row as $partner) {
+		echo '<div class="partner">';
+		echo '<img src="'.$partner['img'].'" alt="pic">';
+		echo $partner['name'];
+		echo $partner['location'];
+		echo '<a href="http://'.$partner['site'].'">'.$partner['site'].'</a>';
+		echo '</div>';
+	}
+	// print_r($row);
+	?>
+</div>
